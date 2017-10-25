368c3272543d77686e8a0bcea9c6028bf1a5b7fc
andrerosamatos <andre.matos@ccc.ufcg.edu.br>
2017-10-25 17:38:00 -0300

    RR03

---
+++ RR3-01/src/main/java/recursao/MetodosRecursivos.java
@@ -0,0 +1,77 @@
+package recursao;
+
+public class MetodosRecursivos {
+
+	public static int calcularSomaArray(int[] array){
+		int i = 0;
+		return calcularSoma(array, i);
+		
+	}
+	private static int calcularSoma(int[] array, int i){
+		int result = 0;
+		if (i <= array.length -1) {
+			result = calcularSoma(array, i+1) + array[i];
+			
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		int[] array = {2};
+		System.out.println(calcularSomaArray(array));
+	}
+	public long calcularFatorial(int n) {
+		long result = 1;
+		
+		
+		return result;
+	}
+
+	public int calcularFibonacci(int n) {
+		int result = 1;
+		// TODO ESCREVA AQUI O CÓDIGO (USANDO RECURSAO) PARA CALCULAR E IMPRIMIR
+		// O N-ESIMO TERMO
+		// DA SEQUENCIA DE FIBONACCI, QUE TEM A SEGUINTE LEI DE FORMACAO: O
+		// PRIMEIRO E SEGUNDO NUMEROS
+		// DA SEQUENCIA SÃO 1. A PARTIR DO TERCEIRO TERMO, CADA TERMO DA
+		// SEQUENCIA É DADO
+		// PELA SOMA DOS OUTROS DOIS ANTERIORES. OBSERVE QUE SENDO O METODO
+		// RECURSIVO, O FIBONACCI DOS NUMEROS ANTERIORES TAMBEM VAO SER
+		// IMPRESSOS
+		return result;
+	}
+
+	public int countNotNull(Object[] array) {
+		int result = 0;
+		// TODO IMPLEMENTE AQUI O CODIGO QUE CONTA (USANDO RECURSAO) A
+		// QUANTIDADE DE ELEMENTOS NAO NULOS
+		// DE UM ARRAY DE OBJETOS RECEBIDO COMO PARAMETRO
+		return result;
+	}
+
+	public long potenciaDe2(int expoente) {
+		int result = 1;
+		// TODO IMPLEMENTE (USANDO RECURSAO) O CODIGO PARA PRODUZIR A N-ESIMA
+		// POTENCIA
+		// DE 2
+		return result;
+	}
+
+	public double progressaoAritmetica(double termoInicial, double razao, int n) {
+		double result = 0;
+		// TODO IMPLEMENTE SEU CODIGO (USANDO RECURSAO) DE ENCONTRAR O n-ESIMO
+		// TERMO
+		// DA PROGRESSAO ARITMETICA, DADO O TERMO INICIAL E A RAZAO
+		return result;
+	}
+
+	public double progressaoGeometrica(double termoInicial, double razao, int n) {
+		double result = 1;
+		// TODO IMPLEMENTE SEU CODIGO (USANDO RECURSAO) DE ENCONTRAR O n-ESIMO
+		// TERMO
+		// DA PROGRESSAO GEOMETRICA, DADO O TERMO INICIAL E A RAZAO
+		return result;
+	}
+	
+	
+}
